73068f8f6912a949c4741814bbd5ed2631168e13
Eddie Gustafsson <eddie.gustafsson@elev.ga.ntig.se>
2019-10-27 18:29:40 +0100

    Added search.

--- api/objects/users.php
+++ api/objects/users.php
@@ -30,5 +30,35 @@ class Users{
     
         return $stmt;
     }
+
+    // search products
+    function search($keywords){
+    
+        // select all query
+        $query = "SELECT * FROM `anvandare`
+                FROM
+                    " . $this->table_name . "
+                WHERE
+                    anamn LIKE ? OR id LIKE ? OR email LIKE ?
+                ORDER BY
+                    id DESC";
+    
+        // prepare query statement
+        $stmt = $this->conn->prepare($query);
+    
+        // sanitize
+        $keywords=htmlspecialchars(strip_tags($keywords));
+        $keywords = "%{$keywords}%";
+    
+        // bind
+        $stmt->bindParam(1, $keywords);
+        $stmt->bindParam(2, $keywords);
+        $stmt->bindParam(3, $keywords);
+    
+        // execute query
+        $stmt->execute();
+    
+        return $stmt;
+    }
 }
 ?>
