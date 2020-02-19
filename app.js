5b22c82e00b02b060847a875a2527dba4fd378e1
Eddie Gustafsson <eddie.gustafsson@elev.ga.ntig.se>
2020-02-19 09:52:30 +0100

    Changed route name from wikis to wiki.

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.use((req, res, next) => {
 });
 
 //Routes which should handle requests
-app.use('/wikis', wikiRoutes);
+app.use('/wiki', wikiRoutes);
 app.use('/services', serviceRoutes);
 app.use('/upload', uploadRoutes);
 app.use('/user', userRoutes);
