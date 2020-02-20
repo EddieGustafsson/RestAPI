914ef3df772e1f61c036dce2b08466b08d4ae87a
Eddie Gustafsson <eddie.gustafsson@elev.ga.ntig.se>
2020-02-20 10:56:17 +0100

    Changed the standard port to 80

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const app = require('./app');
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 80;
 
 const server = http.createServer(app);
 
