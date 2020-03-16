470ecd0e82349239bed099b20e443116efea7b95
Eddie Gustafsson <eddie.gustafsson@elev.ga.ntig.se>
2020-03-16 17:55:52 +0100

    Changed default port.

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const app = require('./app');
 
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 8888;
 
 const server = http.createServer(app);
 
